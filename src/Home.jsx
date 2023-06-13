9bca5251051442eda5e17e54dd71525f18cd6572
Alessio Ferretti <alessio.ferretti@bwz-rappi.ch>
2023-06-13 06:45:00 +0200

    user-thing should completely work now

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,5 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth"
-import { useCollectionData, useCollection } from "react-firebase-hooks/firestore"
 import { auth, db } from "./db/firebase";
 import { collection, query, onSnapshot } from "firebase/firestore";
 import { Chat } from "./components/Chat";
@@ -19,50 +18,61 @@ export function Home(){
 
 
     useEffect(()=>{
-        const unsubscribe = onSnapshot(queryChats, (querySnapshot) => {
-            querySnapshot.docChanges().forEach((change) => {
-              // Following check for user in db works!!!
-              // change.doc.data().users.indexOf("LrOqrJaaqUdDZIuYxcv0xEp80E2")>-1
-              if(change.doc.data().users.indexOf("LrOqrJaaqUdDZI9uYxcv0xEp80E2")>-1){
-                let changedChat = { id: change.doc.id, ...change.doc.data() };
-        
-                if (change.type === "added") {
-                  addChat(changedChat);
-                }
-                if (change.type === "modified") {
-                  console.log("Modified message: ", change.doc.data());
-                }
-                if (change.type === "removed") {
-                  console.log("Removed message: ", change.doc.data());
-                }
+      const unsubscribe = onSnapshot(queryChats, (querySnapshot) => {
+          querySnapshot.docChanges().forEach((change) => {
+            // Following check for user in db works!!!
+            // change.doc.data().users.indexOf("LrOqrJaaqUdDZIuYxcv0xEp80E2")>-1
+            if(change.doc.data().users.indexOf(user?.uid)>-1){
+              let changedChat = { id: change.doc.id, ...change.doc.data() };
+      
+              if (change.type === "added") {
+                addChat(changedChat);
               }
-            });
+              if (change.type === "modified") {
+                console.log("Modified message: ", change.doc.data());
+              }
+              if (change.type === "removed") {
+                console.log("Removed message: ", change.doc.data());
+                deleteChat(changedChat);
+              }
+            }
           });
-        
-          return () => {
-            unsubscribe(); // Unsubscribe when the component is unmounted or dependency changes
-          };
-    },[])
+        });
+      
+      return () => {
+        unsubscribe(); // Unsubscribe when the component is unmounted or dependency changes
+      };
+  },[user])
 
-    function addChat(chat){
-        setChats(currentChats=>{
-            return[
-                ...currentChats,
-                {name:chat.name,users:chat.users,id:chat.id}
-            ]
-        })
-    }
 
-    function setChat(chat){
-        console.log("setChatExecuted")
-        setActiveChat(chat)
-    }
 
-    return(
-        <>
-            {chats && <ChatList chats={chats} setChat={setChat}/>}
-            <h1>========================================</h1>
-            {activeChat && <ActiveChat chat={activeChat}/>}
-        </>
-    )
+  function addChat(chat){
+    setChats(currentChats=>{
+        return[
+            ...currentChats,
+            {name:chat.name,users:chat.users,id:chat.id}
+        ]
+    })
+  }
+  function deleteChat(chat){
+    setChats(currentChats=>{
+      return currentChats.filter(theChatToDelete => theChatToDelete.id !== chat.id)
+    })
+  }
+
+
+
+
+  function setChat(chat){
+      console.log("setChatExecuted")
+      setActiveChat(chat)
+  }
+
+  return(
+      <>
+          {chats && <ChatList chats={chats} setChat={setChat}/>}
+          <h1>========================================</h1>
+          {activeChat && <ActiveChat chat={activeChat}/>}
+      </>
+  )
 }
