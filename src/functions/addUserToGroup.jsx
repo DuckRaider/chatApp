a8d871b58efbec0b3498776e7aa967eb48199ab3
Alessio Ferretti <alessio.ferretti@bwz-rappi.ch>
2023-06-27 06:42:17 +0200

    adding user works

--- src/functions/addUserToGroup.jsx
+++ src/functions/addUserToGroup.jsx
@@ -2,7 +2,12 @@ import { doc, updateDoc } from "firebase/firestore";
 import { db } from "../db/firebase";
 
 export async function addUserToGroup(chat, uid){
-    chat.users.push(uid)
-    const chatRef = doc(db, "chats", chat.id)
-    await updateDoc(chatRef, chat)
+    // check if user is already in group
+    if(chat.users.indexOf(uid) == -1){
+        chat.users.push(uid)
+        const chatRef = doc(db, "chats", chat.id)
+        await updateDoc(chatRef, chat)
+    }else{
+        alert("User is already in group")
+    }
 }
